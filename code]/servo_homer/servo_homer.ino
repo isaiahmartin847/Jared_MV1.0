dc8b62f6ae27c3d22c06d25e50b310120ec780f8
isaiah <isaiahmartin847@gmail.com>
2022-07-24 12:13:08 -0600

    bearing a gcode code update

--- code]/servo_homer/servo_homer.ino
+++ code]/servo_homer/servo_homer.ino
@@ -5,34 +5,66 @@ Servo servoP1;
 Servo servoP2;
 
 const int Home = 0;
-const int Lpos = 270; 
-int pos = 0;
+int posY = 0;
+int posP1 = 0;
+int posP2 = 0;
+const int full = 270;
+
 
 void setup() {
   // put your setup code here, to run once:
-  servoY.attach(7);
-  servoP1.attach(8);
-  servoP2.attach(9);
-  
+  servoY.attach(9);
+  servoP1.attach(10);
+  servoP2.attach(11);
+
+  servoY.write(Home);
+  delay(5);
+  servoP1.write(Home);
+  delay(5);
+  servoP2.write(Home);
+  delay(5);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  servoY.write(Home);
-  servoP1.write(Home);
-  servoP2.write(Home);
-
-   for (int pos = 0;pos <= 270; pos += 1)
-   {
-    servoY.write(pos);
-    delay (15);
-   }
-
-   servoY.write(Home);
-   delay(30);
-  
 
   
+//sero Y
+for (int pos = 0; pos <= full; pos += 1) 
+  {
+      servoY.write(pos);              
+      delay(10);                        
+  }
+  
+  for (int pos = 270; pos >= 0; pos -= 1) 
+  { 
+      servoY.write(pos);              
+      delay(10); 
+  }
 
+  //servo P1
+  for (int pos = 0; pos <= full; pos += 1) 
+  {
+      servoP1.write(pos);              
+      delay(10);                    
+  }
+  
+  for (int pos = 270; pos >= 0; pos -= 1) 
+  { 
+      servoP1.write(pos);              
+      delay(10); 
+  }
+
+  //servoP2
+  for (int pos = 0; pos <= full; pos += 1) 
+  {
+     servoP2.write(pos);              
+      delay(10);                       
+  }
+  
+  for (int pos = 270; pos >= 0; pos -= 1) 
+  { 
+      servoP2.write(pos);              
+      delay(10); 
+  }
 }
